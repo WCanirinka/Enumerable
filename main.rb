2deab5712c81c4e549df722544a70fe8c4ff1063
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-22 00:48:18 +0200

    Added my_select method

--- main.rb
+++ main.rb
@@ -11,12 +11,22 @@ module Enumerable
     # my_each([1,2,3,4]) do |x|
         # x * 2
     # end
-    
+
     def my_each_with_index(array)
         num = []
         x = 0
         while x < array.length
             num << yield(array[x], x)
+            x += 1
+        end
+        return num
+    end
+
+    def my_select(array)
+        num = []
+        x = 0
+        while x < array.length
+            num << yield(array[x])
             x += 1
         end
         return num

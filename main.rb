49f17fdfd3024e4202a6ca75df222e479232a4ce
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-22 00:36:36 +0200

    Added my_each and my_each_with_index to the main.rb file

---
+++ main.rb
@@ -0,0 +1,24 @@
+module Enumerable
+    def my_each(array)
+        num = []
+        x = 0
+        while x < array.length
+            num << yield(array[x])
+            x += 1
+        end
+        return num
+    end
+    # my_each([1,2,3,4]) do |x|
+        # x * 2
+    # end
+    
+    def my_each_with_index(array)
+        num = []
+        x = 0
+        while x < array.length
+            num << yield(array[x], x)
+            x += 1
+        end
+        return num
+    end
+end

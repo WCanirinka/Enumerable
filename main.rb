8042276ebf04497cf809e06f42a041cc34f10c5a
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-22 18:11:16 +0200

    Made some changes in the main.rb

--- main.rb
+++ main.rb
@@ -25,8 +25,8 @@ module Enumerable
     num = []
     x = 0
     while x < array.length
-      if array[x]
-        num << yield(array[x])
+      if yield(array[x])
+        num << array[x]
       end
       x += 1
     end

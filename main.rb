7bfa3a02f166dfddaf806ced8bbba13ba7bed6a5
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-22 17:28:25 +0200

    Added more tests

--- main.rb
+++ main.rb
@@ -1,31 +1,33 @@
 module Enumerable
-    def my_each(array)
-        num = []
-        x = 0
-        while x < array.length
-            num << yield(array[x])
-            x += 1
-        end
-        return num
+  def my_each(array)
+    num = []
+    x = 0
+    while x < array.length
+      num << yield(array[x])
+      x += 1
     end
+    return num
+  end
 
-    def my_each_with_index(array)
-        num = []
-        x = 0
-        while x < array.length
-            num << yield(array[x], x)
-            x += 1
-        end
-        return num
+  def my_each_with_index(array)
+    num = []
+    x = 0
+    while x < array.length
+      num << yield(array[x], x)
+      x += 1
     end
+    return num
+  end
 
-    def my_select(array)
-        num = []
-        x = 0
-        while x < array.length
-            num << yield(array[x])
-            x += 1
-        end
-        return num
+  def my_select(array)
+    num = []
+    i\x = 0
+    while x < array.length
+      if yield(array[x])
+        num << array[x]
+      end
+      x += 1
     end
+    return num
+  end
 end

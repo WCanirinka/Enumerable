7df0c440da08467cda0450b76ec2ce502a7a7c08
Wilfried Canirinka <canirinkawilfried@gmail,com>
2019-11-25 21:27:21 +0200

    Changes made to the main.rb

--- main.rb
+++ main.rb
@@ -24,7 +24,7 @@ module Enumerable
   end
 
   def my_select
-    return to_enum(method :(my_each)) if !block_given?
+    return to_enum(method = :my_each) if !block_given?
 
     arr = []
     x = 0
